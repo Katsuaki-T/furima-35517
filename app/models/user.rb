2e5b8e86b6f0e655003e47b20fb35ca27903b38f
Katsuaki-T <cxq84403@gmail.com>
2021-05-04 22:49:21 +0900

    column→migrate, validate(model)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  validates :nickname, presence: true
+  validates :first_name , presence: true
+  validates :family_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :family_name_kana, presence: true
+  validates :birth_day, presence: true
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+
+
 end
