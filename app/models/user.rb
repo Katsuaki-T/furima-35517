75370053ddab334d700784a62f5f0b74440e1941
Katsuaki-T <cxq84403@gmail.com>
2021-05-05 19:13:47 +0900

    validates 半角英数〜ひらカタ漢字

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,20 @@ class User < ApplicationRecord
     validates :birthday
   end
 
+  validates :password, format: { with: /\A[a-z0-9]+\z/i }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :family_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :first_name_kana
+    validates :family_name_kana
+  end
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
 end
