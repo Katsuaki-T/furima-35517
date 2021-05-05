9a3fff6ac2b130e600b23597c7d8cca76b17b669
Katsuaki-T <cxq84403@gmail.com>
2021-05-05 17:37:51 +0900

    メールアドレスが一意性であること

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,12 @@ FactoryBot.define do
     email                 {'test@example'}
     password              {'1q1q1q'}
     password_confirmation {password}
+    nickname              {"ai"}
+    first_name            {"あい"}
+    family_name           {"あいだ"}
+    first_name_kana       {"アイ"}
+    family_name_kana      {"アイダ"}
+    birthday              {19300101}
+
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,11 @@ RSpec.describe User, type: :model do
     end
 
     it 'メールアドレスが一意性であること' do
-      
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
 
     it 'メールアドレスは、@を含む必要があること' do
