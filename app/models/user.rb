8c37d0d0ebd7489bd985db83dbb1363cd29452fe
Katsuaki-T <cxq84403@gmail.com>
2021-05-05 18:59:45 +0900

    with_options validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :family_name_kana, presence: true
-  validates :birthday, presence: true
+
+  with_options presence: true do
+    validates :nickname
+    validates :first_name
+    validates :family_name
+    validates :first_name_kana
+    validates :family_name_kana
+    validates :birthday
+  end
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
