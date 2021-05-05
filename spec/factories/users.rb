222da5869508a097b3e62d43d526b33062af1bb8
Katsuaki-T <cxq84403@gmail.com>
2021-05-05 16:52:58 +0900

    pass rename

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     email                 {'test@example'}
-    password              {'000000'}
+    password              {'1q1q1q'}
     password_confirmation {password}
   end
 end
