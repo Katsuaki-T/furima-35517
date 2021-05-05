e274471bcffb01a2bf1975db46d69ed02ea6498f
Katsuaki-T <cxq84403@gmail.com>
2021-05-05 13:55:15 +0900

    model birthday fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :family_name, presence: true
   validates :first_name_kana, presence: true
   validates :family_name_kana, presence: true
-  validates :birth_day, presence: true
+  validates :birthday, presence: true
 
 
   devise :database_authenticatable, :registerable,
