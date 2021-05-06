0422d766360205df6e5c0d0fe9b4213c9423efb4
Katsuaki-T <cxq84403@gmail.com>
2021-05-06 20:29:35 +0900

    java calc price

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,5 +1,15 @@
 function calc() {
-console.log("hello")
+const itemPrice = document.getElementById("item-price")
+itemPrice.addEventListener("input",() => {
+const itemPriceValue = itemPrice.value
+const tax = 0.1
+const taxFee = itemPriceValue * tax
+const profit = itemPriceValue - taxFee
+const taxArea = document.getElementById("add-tax-price")
+const profitArea = document.getElementById("profit")
+taxArea.innerHTML = taxFee
+profitArea.innerHTML = profit
+})
 }
 
 
