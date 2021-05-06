8882d7b7e0fdee854c43018632b969adefc2a329
Katsuaki-T <cxq84403@gmail.com>
2021-05-06 21:16:37 +0900

    item test ~ FactoryBot

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品機能' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+context '内容に問題がない場合' do
+  
+end
+
+context '内容に問題がある場合' do
+
+end 
+
+
+
+  end
 end
