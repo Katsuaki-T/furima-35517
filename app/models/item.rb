e67f440ae76c884e94f9fd63acde5c25d33a51b0
Katsuaki-T <cxq84403@gmail.com>
2021-05-06 14:19:29 +0900

    items validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,22 @@
 class Item < ApplicationRecord
-  
+
+    with_options presence: true do
+      validates :name
+      validates :description
+      validates :image
+      validates :price, format: { with: /^[0-9]+$/ }, inclusion: { in: 300..9_999_999 }
+    end
+
+
+   with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :delivery_time_id
+  end
+
+
+
+    
 end
