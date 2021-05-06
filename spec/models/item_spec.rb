8d7b47c6ea345fac94118107ad30ad758f61d4e6
Katsuaki-T <cxq84403@gmail.com>
2021-05-06 21:17:56 +0900

    内容に問題がない場合

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,7 +6,10 @@ RSpec.describe Item, type: :model do
       @item = FactoryBot.build(:item)
     end
 context '内容に問題がない場合' do
-  
+
+  it '全ての商品があれば出品できる場合' do
+    expect(@item).to be_valid
+  end
 end
 
 context '内容に問題がある場合' do
