66e7a574d35195469c7853719b6711d905033aa4
Katsuaki-T <cxq84403@gmail.com>
2021-05-08 17:00:27 +0900

    attr_accessor

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,4 +1,5 @@
 class PurchaseAddress
   include ActiveModel::Model
-  
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+               ,:purchase_history_id, :user_id, :item_id
 end
