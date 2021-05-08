9739d5e25541fb9eaea65ec9266f9e9ee9454f3a
Katsuaki-T <cxq84403@gmail.com>
2021-05-08 17:20:53 +0900

    presence true

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,4 +2,16 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
                ,:purchase_history_id, :user_id, :item_id
+
+ with_options presence: true do   
+  validates :postal_code 
+  validates :prefecture_id 
+  validates :city
+  validates :house_number 
+  validates :phone_number 
+  validates :purchase_history_id
+  validates :user_id
+  validates :item_id
+ end
+
 end
