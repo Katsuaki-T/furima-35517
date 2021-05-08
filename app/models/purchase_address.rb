2b46851fcb51b5c87f2523b2cb544f9b8dfaf12c
Katsuaki-T <cxq84403@gmail.com>
2021-05-08 19:30:34 +0900

    validates fixing

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,15 +1,15 @@
 class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
-               ,:purchase_history_id, :user_id, :item_id
+              , :user_id, :item_id
 
  with_options presence: true do   
-  validates :postal_code 
-  validates :prefecture_id 
+  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+  validates :prefecture_id ,numericality: { other_than: 1 }
   validates :city
   validates :house_number 
-  validates :phone_number 
-  validates :purchase_history_id
+  validates :phone_number, format: {with: /\A\d{11}\z/ } 
+  
   validates :user_id
   validates :item_id
  end
