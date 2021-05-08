95bec3368dadd6a74e871d4f9d918242a8f724d1
Katsuaki-T <cxq84403@gmail.com>
2021-05-08 14:41:26 +0900

    destroy set_item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
-  before_action :move_to_index, except: [:index, :show, :new, :create, :destroy]
+  before_action :set_item, only: [:edit, :show, :update,:destroy]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -57,6 +57,9 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
+    redirect_to action: :index 
+    end
   end
+
 end
