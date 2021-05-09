35ac5671722fd2830dd11622a367fde77d72620c
Katsuaki-T <cxq84403@gmail.com>
2021-05-09 19:44:50 +0900

    item create

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -16,7 +16,7 @@ class PurchaseAddress
  def save
   purchase_history = PurchaseHistory.create(user_id: user_id, item_id: item_id)
 
-  sendind_address = SendingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id,city: city, house_number: house_number, building_name: building_name, phone_number: phone_number,purchase_history_id: purchase_history_id) 
+  sendind_address = SendingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id,city: city, house_number: house_number, building_name: building_name, phone_number: phone_number,purchase_history_id: purchase_history.id) 
 
  end
 
