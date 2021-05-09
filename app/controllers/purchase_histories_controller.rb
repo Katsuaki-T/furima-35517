bf7423c0ee9779ba4bb5ba2101ca132d745d3c1e
Katsuaki-T <cxq84403@gmail.com>
2021-05-09 13:25:19 +0900

    purchase_history #create draft

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -4,6 +4,22 @@ class PurchaseHistoriesController < ApplicationController
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(purchase_history_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+
+ private
+
+  def purchase_history_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id
+      ,:city, :house_number, :building_name, :phone_number,).merge(user_id: current_user.id, item_id: ？.id)
+  end
+
+
 end
