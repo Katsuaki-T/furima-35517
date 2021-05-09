1bb6ee9b5e3baa7346a4e4cb1e8985fb49eb7aad
Katsuaki-T <cxq84403@gmail.com>
2021-05-09 14:23:44 +0900

    item id stand-by state

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,6 +1,7 @@
 class PurchaseHistoriesController < ApplicationController
 
   def index
+    @purchase_address = PurchaseAddress.new
   end
 
   def create
@@ -18,7 +19,7 @@ class PurchaseHistoriesController < ApplicationController
 
   def purchase_history_params
     params.require(:purchase_address).permit(:postal_code, :prefecture_id
-      ,:city, :house_number, :building_name, :phone_number,).merge(user_id: current_user.id, item_id: ？？.id)
+      ,:city, :house_number, :building_name, :phone_number,).merge(user_id: current_user)
   end
 
 
