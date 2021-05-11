66ddc814a70d11c302002dd65eb21bb0b47b92e9
Katsuaki-T <cxq84403@gmail.com>
2021-05-11 11:38:04 +0900

    factory building_name

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     prefecture_id { 2 }
     city { 'city' }
     house_number { '1-2' }
+    building_name { 'tatemono' }
     phone_number { '09012345678' }
     token { 'tok_abcdefghijk00000000000000000' }
   end
