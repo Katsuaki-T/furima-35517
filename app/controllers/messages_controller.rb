306b0b35ab9ab5d61874cc3147ace3b64947da83
Katsuaki-T <cxq84403@gmail.com>
2021-05-15 21:48:37 +0900

    broadcast

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,8 +5,9 @@ class MessagesController < ApplicationController
   end
 
   def create
-
-    @message = Message.new(text: params[:message][:text])
-    
+   @message = Message.new(text: params[:message][:text])
+   if @message.save
+    ActionCable.server.broadcast 'message_channel', content: @message
+  end
   end
 end
