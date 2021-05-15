44de1bce023c77d9b8fedd83b7464f23f4f9d14e
Katsuaki-T <cxq84403@gmail.com>
2021-05-15 20:24:24 +0900

    message model controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'messages/new'
   devise_for :users
   root 'items#index'
 
 resources :items do
   resources :purchase_histories ,only: [:index, :create]
 end
+
+resources :messages, only: [:create]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_035116) do
+ActiveRecord::Schema.define(version: 2021_05_15_112058) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 2021_05_09_035116) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "messages", charset: "utf8", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "purchase_histories", charset: "utf8", force: :cascade do |t|
