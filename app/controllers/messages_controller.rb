3dba034d2e43d62b559a864ba09aabec02dec7c3
Katsuaki-T <cxq84403@gmail.com>
2021-05-16 14:35:48 +0900

    message_params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,9 +5,16 @@ class MessagesController < ApplicationController
   end
 
   def create
-   @message = Message.new(text: params[:message][:text])
+   @message = Message.new(message_params)
    if @message.save
     ActionCable.server.broadcast 'message_channel', content: @message
   end
   end
+
+private
+
+def message_params
+  params.require(:message).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+end
+
 end
