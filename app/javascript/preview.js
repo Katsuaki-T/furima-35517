1628556586319d808afa8b75d6eaba840a305ab7
Katsuaki-T <cxq84403@gmail.com>
2021-05-18 09:38:17 +0900

    if (imageContent) remove

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,6 +2,13 @@ document.addEventListener('DOMContentLoaded', function(){
   if ( document.getElementById('message_image')){
   const ImageList = document.getElementById('image-list');
   document.getElementById('item-image').addEventListener('change', function(e){
+
+    // 画像が表示されている場合のみ、すでに存在している画像を削除する
+    const imageContent = document.querySelector('img');
+    if (imageContent){
+      imageContent.remove();
+    }
+    
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
 
